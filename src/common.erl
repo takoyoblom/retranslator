daf4b21a289f9ac298e0e90aefaf25a7296e405a
takoyoblom <takoy@oblom.com>
2010-06-21 18:40:11 +0300

    ugu

--- src/common.erl
+++ src/common.erl
@@ -2,14 +2,14 @@
 -author('author <takoy@oblom.com>').
 
 -export([get_conf/2]).
-
+% EXPORTED FUNCTIONS
 get_conf(Key, Default) ->
     case application:get_env(safe_key(Key)) of
         {ok, Data} -> Data;
         undefined -> Default
     end.
 
-
+%% INTERNUL FUNCTIONS
 safe_key(Key) when is_atom(Key) ->
     Key;
 safe_key(Key) when is_list(Key) ->
